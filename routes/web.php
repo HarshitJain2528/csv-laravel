2cd791c35ad32f530eb94d82891e3f3b904463d2
HarshitJain2528 <hj252214@gmail.com>
2023-10-19 23:35:34 +0530

    files added

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Csv;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [Csv::class,'index']);
+Route::get('/export_csv', [Csv::class,'export_csv'])->name('export.csv');
